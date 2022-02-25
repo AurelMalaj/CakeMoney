deb1e87493fc26a398c05e166097a356411fb437
AlexandroBur <alexan94dro@gmail.com>
2022-02-25 16:57:12 +0100

    allinemanto classe wallet

--- lib/model/wallet.dart
+++ lib/model/wallet.dart
@@ -2,10 +2,26 @@ import 'package:cakemoney/model/movement.dart';
 
 class Wallet {
   final List<Movement> movements;
-  
+
   Wallet({
     required this.movements,
   });
 
-  
+  double sum() {
+    double sum = 0;
+    for (var values in movements) {
+      sum = sum + values.value;
+    }
+    return sum;
+  }
+
+  double sumByCategory(String cat) {
+    double sum = 0;
+    for (var moves in movements) {
+      if (moves.category == cat) {
+        sum = sum + moves.value;
+      }
+    }
+    return sum;
+  }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:cakemoney/screens/entrance.dart';
-import 'package:flutter/material.dart';
 import 'package:cakemoney/model/movement.dart';
+import 'package:cakemoney/model/wallet.dart';
+import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title, List<Movement>? wallet})
@@ -14,7 +15,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //Variabile per creazione di una lista wallet di movement da avere disponible nel widget
   List<Movement> _wallet = [];
+  //Variabile per visualizzare a schermo l'iserimento test di un valore di esempio in _wallet
   String _testo = "";
 
   @override
@@ -23,45 +26,47 @@ class _MyHomePageState extends State<MyHomePage> {
     _wallet = widget.wallet;
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-          backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(_testo),
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(_testo),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (() {
-            setState(() {
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (
+            /* --- Prova di display dati dalla classe wallet-Movement
+            () {
+            setState(
+              () {
               _wallet.add(Movement(value: 33, category: "banane"));
               _testo = _wallet[0].category;
             });
-          }
-              /*() {
+          }*/
+            () {
           Navigator.push(
             context,
             MaterialPageRoute(
                 builder: (context) => const Entrance(
                       title: "Prova",
-                    )
-                    ),*/
-              ),
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ));
+                    )),
+          );
+        }),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
