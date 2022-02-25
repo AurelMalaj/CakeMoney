a8b7933dbc8435e36ddae27f5a44ecea8a819f5f
AlexandroBur <alexan94dro@gmail.com>
2022-02-25 18:02:44 +0100

    modifche ad entrance al 2502

--- lib/screens/entrance.dart
+++ lib/screens/entrance.dart
@@ -1,29 +1,48 @@
-import 'package:cakemoney/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:cakemoney/model/movement.dart';
 
-class Entrance extends StatelessWidget {
+class Entrance extends StatefulWidget {
   const Entrance({Key? key, required this.title}) : super(key: key);
-
-  void bottomBarRoute(BuildContext context, Widget widget) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => widget));
-  }
-
   final String title;
+
+  @override
+  _EntranceState createState() => _EntranceState();
+}
+
+class _EntranceState extends State<Entrance> {
+  // MockTest per display info in ListView
+  final List<Movement> mockMovement = <Movement>[
+    Movement(value: 23.03, category: "pinoli"),
+    Movement(value: 606.80, category: "pinguini")
+  ];
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
-        )), // list? // card?
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: entries.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 50,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(
+                child: Text(
+                    'Entry ${entries[index]}')), // per andare a capo ( o childer o /n)
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
+
+      // list? // card?
+
       bottomNavigationBar:
           BottomNavigationBar(items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
