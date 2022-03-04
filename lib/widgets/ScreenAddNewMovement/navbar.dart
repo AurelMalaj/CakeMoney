292d14e453c97d221d90c22c3890813a1ed13fff
NicoGorini <gorininicolas12@gmail.com>
2022-03-04 14:50:30 +0100

    ..

---
+++ lib/widgets/ScreenAddNewMovement/navbar.dart
@@ -0,0 +1,63 @@
+import 'package:cakemoney/screens/entrance.dart';
+import 'package:cakemoney/screens/home.dart';
+import 'package:flutter/material.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({Key? key, required this.onTap}) : super(key: key);
+  final void Function(int index) onTap;
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      //currentIndex
+      //onTap: widget.onTap,
+      onTap: (index) {
+        if (index == 0) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (builContext) =>
+                      MyHomePage(title: 'Sono una homepage')));
+          // home
+        }
+        if (index == 1) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (builContext) =>
+                      Entrance(title: 'Sono una homepage')));
+
+          // Entrate
+        }
+        if (index == 2) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (builContext) =>
+                      Entrance(title: 'Sono una homepage')));
+          // Uscite
+        }
+      },
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.assignment_turned_in),
+          label: 'Entrate',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.announcement),
+          label: 'Uscite',
+        ),
+      ],
+      selectedItemColor: Color.fromARGB(255, 35, 134, 21),
+    );
+  }
+}
