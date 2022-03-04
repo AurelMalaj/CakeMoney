14cd121ed9147d8ef8d96275a47def200f908397
Francesco <francescosanti123@gmail.com>
2022-03-04 12:08:18 +0100

    modifica buttonAdd

--- lib/widgets/ScreenAddNewMovement/buttonAdd.dart
+++ lib/widgets/ScreenAddNewMovement/buttonAdd.dart
@@ -1,6 +1,8 @@
 //Widget che alla pressione del bottene + nella homepage permette di passare all'inserimento
 //di nuovi movimenti
 
+import 'package:cakemoney/screens/addNewMovement.dart';
+import 'package:cakemoney/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'buttonSelectCategory.dart';
@@ -10,48 +12,50 @@ class ButtonAdd extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        child: const Text(
-          '+',
-          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
-        ),
-        onPressed: () {
-          showModalBottomSheet<void>(
-            context: context,
-            builder: (BuildContext context) {
-              return Container(
-                height: 700,
-                color: Color.fromARGB(255, 156, 155, 152),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 16),
-                        child: TextField(
-                          decoration:
-                              InputDecoration(labelText: "Enter your number"),
-                          keyboardType: TextInputType.number,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                        ),
-                      ),
-                      const ButtonSelectCategory(),
-                      ElevatedButton(
-                        child: const Text('Add'),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                    ],
-                  ),
+    return Container(
+      height: 700,
+      color: Color.fromARGB(255, 156, 155, 152),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(labelText: "Enter your number"),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+              ),
+            ),
+            //seleziona categoria
+            const ButtonSelectCategory(),
+            //Inserimento note
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'note',
                 ),
-              );
-            },
-          );
-        },
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Add'),
+              onPressed: (() {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => MyHomePage(
+                            title: 'CakeMoney',
+                          )),
+                );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
